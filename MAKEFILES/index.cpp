67b471cd1e2db62d5977e9fb4e1106f8ce2d6abf
Luis Enrique <luisenriquecgongora@gmail.com>
2019-10-22 00:44:26 +0200

    Added makefile sample

---
+++ MAKEFILES/index.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Account.h"
+
+using namespace std;
+
+int main()
+{
+    vector <string> report;
+    Account a1;
+    a1.Deposit(90);
+    a1.FullReport();
+    a1.Withdraw(50);
+    a1.FullReport();
+    cout << a1.getBalance() <<endl;
+
+}
